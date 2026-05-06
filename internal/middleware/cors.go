1ef59b3bf0b7f54b1bcffa44fca9f8cc87e82ec6
Mantusahoo07 <mantusahoo343@gmail.com>
2026-05-06 10:15:02 +0530

    Update CORS to allow localhost frontend

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -7,11 +7,26 @@ import (
 
 func CORS(frontendURL string) gin.HandlerFunc {
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{frontendURL, "http://localhost:3000", "http://localhost:5173"}
+	config.AllowOrigins = []string{
+		frontendURL,
+		"http://localhost:3000",
+		"http://localhost:3001",
+		"http://localhost:5173",
+		"http://127.0.0.1:3000",
+		"https://pos-backend-h2c3.onrender.com",
+		"https://pos-proxy.d98restaurant.workers.dev",
+	}
 	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"}
-	config.AllowHeaders = []string{"Origin", "Content-Type", "Accept", "Authorization", "X-Requested-With"}
+	config.AllowHeaders = []string{
+		"Origin",
+		"Content-Type",
+		"Accept",
+		"Authorization",
+		"X-Requested-With",
+		"X-Forwarded-For",
+	}
 	config.AllowCredentials = true
 	config.ExposeHeaders = []string{"Content-Length", "Authorization"}
-
+	
 	return cors.New(config)
 }
